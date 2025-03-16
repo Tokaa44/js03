8409c2f32a49de46b20b0141b3393841f94d5089
Tokaa44 <tokamoustafa44@gmail.com>
2025-03-16 23:56:42 +0200

    the rest of w7

--- js7.js
+++ js7.js
@@ -89,3 +89,108 @@ for (let j = start2; j <= end2; j++) {
 
 console.log("-----------");
 //----------------------------------
+//1.4
+
+let index = 10;
+let jump = 2;
+
+for (;;) {
+  console.log(index);
+  index -= jump;
+  if (index < jump *jump) {
+    break;
+  }
+}
+
+// Output
+10
+8
+6
+4
+
+console.log("-----------");
+//----------------------------------
+//1.5 ?
+
+let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
+let letter = "a";
+
+num=0;
+for (let x = 0; x < friends.length; x++) {
+  if (friends[x].toLocaleLowerCase().startsWith(letter)) {
+    continue;
+  }  
+  else{
+    num++;
+    console.log(`${num} => ${x}`);
+  }
+}
+
+// Output
+"1 => Sayed"
+"2 => Eman"
+"3 => Mahmoud"
+"4 => Osama"
+"5 => Sameh"
+
+console.log("-----------");
+//----------------------------------
+//1.6
+
+let Start = 0;
+let swappedName = "elZerO";
+let newSN = "";
+
+for (let y = Start; y < swappedName.length; y++) {
+  if (swappedName[y]=== swappedName[y].toLocaleLowerCase()) {
+    newSN += swappedName[y].toLocaleUpperCase();
+  } else{
+    newSN += swappedName[y].toLocaleLowerCase();
+  } 
+}
+console.log(newSN);
+
+
+// Output
+"ELzERo"
+
+console.log("-----------");
+//----------------------------------
+//1.7
+
+let start3 = 0;
+let mix = [1, 2, 3, "A", "B", "C", 4];
+mix.shift();
+
+for (let z = start3; z < mix.length ; z++) {
+  if (typeof(mix[z]) === "number" ) {
+    console.log(mix[z]);
+  }
+}
+
+// Output
+2
+3
+4
+
+console.log("-----------");
+//======================================
+//2.1 ?
+
+let Friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index1 = 0;
+let counter = 0;
+
+while (index1 < Friends.length ) {
+  if (Friends[index1].startsWith("A") || typeof(Friends[index1]) === "number") {
+    index1 ++;
+    continue
+  }
+  counter++;
+  console.log(`${counter} => ${Friends[index1]}`);
+  index1++;
+}
+
+// Output
+"1 => Sayed"
+"2 => Mahmoud"
